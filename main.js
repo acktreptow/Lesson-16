01e8bbd43b99639ee11de7ffd236a9d8ce2b9887
Alex Treptow <acktreptow@gmail.com>
2022-12-07 05:34:44 +0000

    add skills section

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-console.log("Test for Github");
-console.log("final test");
+const here = document.getElementById("here");
+const is = document.getElementById("is");
+const some = document.getElementById("some");
+const random = document.getElementById("random");
+const javascript = document.getElementById("javascript");
+const interactivity = document.getElementById("interactivity");
+
+function red() {
+  here.style.color = "red";
+}
+
+function bold() {
+  is.style.fontWeight = "bold";
+}
+
+function red() {
+  here.style.color = "red";
+}
+
+function red() {
+  here.style.color = "red";
+}
+
+function red() {
+  here.style.color = "red";
+}
+
+function red() {
+  here.style.color = "red";
+}
+
+here.onclick = red;
+is.addEventListener("mouseover", bold);
